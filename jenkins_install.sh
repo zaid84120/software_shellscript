f14fbf53251b2e6aeb4e9c202f9a20b239947ab1
zaid shaikh <zaidshaikh505@gmail.com>
2019-08-28 19:49:14 +0530

    jenkins_installation

---
+++ jenkins_install.sh
@@ -0,0 +1,8 @@
+#!/bin/ksh
+
+sudo apt update
+sudo apt install openjdk-8-jdk
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
