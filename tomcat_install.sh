fe985de5af13448b19c85ad837fbaf24ff2a6938
zaid84120 <44051286+zaid84120@users.noreply.github.com>
2019-08-29 17:59:19 +0530

    Update tomcat_install.sh

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -2,8 +2,8 @@
 sudo apt update
 sudo apt install openjdk-8-jdk -y
 cd /opt
-wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz.asc
-tar -xvzf apache-tomcat-8.5.45.tar.gz.asc
+wget https://www.apache.org/dist/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz
+tar -xvzf apache-tomcat-8.5.45.tar.gz
 chmod +x /opt/apache-tomcat-8.5.45/bin/startup.sh shutdown.sh
 ln -s /opt/apache-tomcat-8.5.45/bin/startup.sh /usr/local/bin/tomcatup
 ln -s /opt/apache-tomcat-8.5.45/bin/shutdown.sh /usr/local/bin/tomcatdown
