ac1025df978d3e4ef1be33e3166c085386144893
Ubuntu <ubuntu@ip-172-31-18-225.us-east-2.compute.internal>
2019-08-29 09:25:34 +0000

    jenkine install modeify

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,8 +1,9 @@
-#!/bin/ksh
+#!/bin/bash
 
 sudo apt update
-sudo apt install openjdk-8-jdk
+sudo apt install openjdk-8-jdk -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
-sudo apt install jenkins
+sudo apt install jenkins -y
+
