84a21271a5c7d38e4eea7b7cb74de36d06faeeae
root <root@ip-172-31-18-225.us-east-2.compute.internal>
2019-08-29 09:41:05 +0000

    updated

---
+++ maven_install.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+mkdir /opt/maven
+cd /opt/maven
+wget http://apachemirror.wuchna.com/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.tar.gz
+tar -xvzf apache-maven-3.6.1-bin.tar.gz
+cd apache-maven-3.6.1
+
